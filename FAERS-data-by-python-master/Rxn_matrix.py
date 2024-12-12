3c18b5750aff7d45da2e6b5d13ed48b527afe595
OedataInsight <ozioma.edokobi1@gmail.com>
2024-12-12 11:02:29 -0500

    Updarted Rxn_matrix
    fixed the code to run the reaction matrix on cleaned_dataset

--- FAERS-data-by-python-master/Rxn_matrix.py
+++ FAERS-data-by-python-master/Rxn_matrix.py
@@ -1,23 +1,28 @@
-
-
 import pandas as pd
 
-# Load your cleaned dataset
-data = pd.read_csv('/Users/oziedokobi/Desktop/BAyesalabproject/faers_xml_2024q3/XML/cleaned_dataset.csv')
-
-# Create a binary indicator for reactions
-data['reaction_flag'] = 1
-
-# Pivot the data to create the reaction matrix
-reaction_matrix = data.pivot_table(
-    index=['primaryid', 'caseid', 'drugname'],  # Rows
-    columns='pt',                               # Columns
-    values='reaction_flag',                     # Values
-    aggfunc='max',                              # Aggregate using max (1 if reaction exists)
-    fill_value=0                                # Fill missing reactions with 0
-).reset_index()
+# Define chunk size for memory efficiency
+chunk_size = 10000
 
 
-# Save to a CSV for use in BayesiaLab or other tools
-# Randomize the rows and limit to 20,000 rows
-reaction_matrix_sampled = reaction_matrix.sample(n=10000, random_state=42)  # Set random_state for reproducibility
+chunks = []
+
+for chunk in pd.read_csv('/kaggle/input/cleaned-dataset/cleaned_dataset.csv', chunksize=chunk_size):
+    # Create a binary indicator for reactions
+    chunk['reaction_flag'] = 1
+    
+    # Pivot 
+    chunk_pivot = chunk.pivot_table(
+        index=['primaryid', 'caseid', 'drugname'],  #
+        columns='pt', 
+        aggfunc='size',  
+        fill_value=0  
+    ).reset_index()
+    chunks.append(chunk_pivot)
+
+
+reaction_matrix = pd.concat(chunks, ignore_index=True)
+
+reaction_matrix_sampled = reaction_matrix.sample(n=20000, random_state=42)
+
+reaction_matrix_sampled.to_csv('reaction_matrix_sampled.csv', index=False)
+print("Sampled reaction matrix saved as 'reaction_matrix_sampled.csv'")
